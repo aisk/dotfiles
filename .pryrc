0dcd0a26c30807ab9e472354e4028678b3533b81
aisk <aisk1988@gmail.com>
2014-04-11 11:40:32 +0800

    add .pryrc

---
+++ .pryrc
@@ -0,0 +1,1 @@
+Pry.config.prompt = [ proc { ">> " }, proc { "?> " }]
--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -sf $(PWD)/.vimrc ~/.vimrc
 ln -sf $(PWD)/.axelrc ~/.axelrc
 ln -sf $(PWD)/.emacs ~/.emacs
 ln -sf $(PWD)/.irbrc ~/.irbrc
+ln -sf $(PWD)/.pryrc ~/.pryrc
 if [ ! -d ~/.vim/bundle ]; then
     git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
