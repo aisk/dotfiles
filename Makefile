fb761b819caa19c46e3f3ee03c22f400bedb9a52
aisk <aisk1988@gmail.com>
2014-04-11 10:54:39 +0800

    change Makefile to bash script

--- Makefile
+++
@@ -1,8 +0,0 @@
-install:
-	ln -sf $(PWD)/.vimrc ~/.vimrc
-	ln -sf $(PWD)/.axelrc ~/.axelrc
-	ln -sf $(PWD)/.emacs ~/.emacs
-	ln -sf $(PWD)/.irbrc ~/.irbrc
-	if [ ! -d ~/.vim/bundle ] do
-		git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-	end
---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env sh
+
+ln -sf $(PWD)/.vimrc ~/.vimrc
+ln -sf $(PWD)/.axelrc ~/.axelrc
+ln -sf $(PWD)/.emacs ~/.emacs
+ln -sf $(PWD)/.irbrc ~/.irbrc
+if [ ! -d ~/.vim/bundle ]; then
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
