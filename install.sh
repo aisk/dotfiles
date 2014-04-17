8783d0120091a89b22daecf79137b2dc098cf9b3
aisk <aisk1988@gmail.com>
2014-04-17 20:07:32 +0800

    fix install script bug

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env sh
 
-ln -sf $(PWD)/.vimrc ~/.vimrc
-ln -sf $(PWD)/.axelrc ~/.axelrc
-ln -sf $(PWD)/.emacs ~/.emacs
-ln -sf $(PWD)/.irbrc ~/.irbrc
-ln -sf $(PWD)/.pryrc ~/.pryrc
+ln -sf $PWD/.vimrc ~/.vimrc
+ln -sf $PWD/.axelrc ~/.axelrc
+ln -sf $PWD/.emacs ~/.emacs
+ln -sf $PWD/.irbrc ~/.irbrc
+ln -sf $PWD/.pryrc ~/.pryrc
 if [ ! -d ~/.vim/bundle ]; then
     git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
