bef7674af98cecf23ed75c5681369e7ca2fc90fe
asaka <aisk1988@gmail.com>
2015-02-04 13:38:37 +0800

    add .ghci

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ ln -sf $BASEDIR/.irbrc ~/.irbrc
 ln -sf $BASEDIR/.pryrc ~/.pryrc
 ln -sf $BASEDIR/.zshrc ~/.zshrc
 ln -sf $BASEDIR/.minttyrc ~/.minttyrc
+ln -sf $BASEDIR/.ghci ~/.ghci
 if [ ! -d ~/.vim/autoload ]; then
     mkdir -p ~/.vim/autoload
     curl -fLo ~/.vim/autoload/plug.vim \
